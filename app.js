62d5e17f2eda7cc7a8ec2556d92164a044cdc077
salanchai <salanchai@nipa.co.th>
2021-04-18 18:46:31 +0700

    init master

---
+++ app.js
@@ -0,0 +1,42 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const bodyparser = require("body-parser");
+const cors = require("cors");
+var indexRouter = require('./routes/index');;
+require('dotenv').config();
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(bodyparser.json());
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen(process.env.PORT || 3004 , () =>
+  console.log(`App listen at port ${process.env.PORT || 3004}`)
+);
